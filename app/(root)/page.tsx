f6b9c5a7c60d7745845787cd58793aef75be3284
Adhil Marzook <adhilmarzook20@gmail.com>
2025-02-17 17:12:52 +0000

    second

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 const Home = () => {
-  return (
-    <div>Home</div>
+    const loggedIn = { firstName: 'Adhil'};
+
+    return (
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn.firstName || 'Guest'}
+                    subtext="Access and manage the account and transactions"
+                />
+
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.25}
+                    
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
